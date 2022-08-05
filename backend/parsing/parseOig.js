b5425af9875cc8f292b89281000b716a32b37970
Jarred Moyer <jarredds941@gmail.com>
2022-08-04 20:23:07 -0400

    Added an isolated file to read OIG ommissions and save a JSON of parsed results to prevent parsing form happening every run

---
+++ backend/parsing/parseOig.js
@@ -0,0 +1,17 @@
+import getData from './formatData.js'
+import fs from 'fs'
+
+async function parse(path){
+    let data = await getData(path,(e)=>{return e.FIRSTNAME})
+    let reformatedResults = []
+    data.forEach(person=>{
+        reformatedResults.push({
+            name: person.LASTNAME + ','+ person.FIRSTNAME + ' ' + person.MIDNAME,
+            DOB:person.DOB
+        })
+    })
+    let saveObj = {data:reformatedResults}
+    let stringy = JSON.stringify(saveObj)
+    fs.writeFileSync('../analysis/oig.json',stringy)
+}
+parse('./UPDATED.csv')
