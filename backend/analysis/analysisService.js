fede210554fb7ff5b2c07cbb25f3b86f43c03e41
Jarred Moyer <jarredds941@gmail.com>
2022-08-04 20:31:57 -0400

    Modified endpoint to use cached OIG Object

--- backend/analysis/analysisService.js
+++ backend/analysis/analysisService.js
@@ -1,7 +1,9 @@
 import fs from 'fs'
-import getData from '../OigData/formatData.js'
+import getData from '../parsing/formatData.js'
 export default async function analyze(path){
-    let oig = await getData('./OigData/UPDATED.csv',(value)=>{return value.FIRSTNAME})
+    let oig = JSON.parse(fs.readFileSync('./analysis/oig.json')).data
     let comp = await getData(path,()=>{return true})
-    console.log(comp)
+    console.log(comp[0])
+    console.log(oig[0])
+    
 }
